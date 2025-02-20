c8366549dfc675926699538f1cc6e59c91769b53
Josh <josh@pop-os.lan>
2025-02-20 13:40:20 -0800

    Added calculation function and classList data structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <list>
 
 /* Definition of for class information */
 
@@ -10,14 +11,39 @@ struct classInfo {
 	std::string letterGrade;
 };
 
+/* List of current grades/classes */
+
+std::list<classInfo> classGradeList;
+
 /* Map for mapping Letter grade to grade point */
 
 std::map<std::string, float> gpm;
 
+/* Sets up the grade point map */
 void setupMap();
+
+/* Addes a grade to the grade list */
+void addToGradeList(std::string name, int credit, std::string letterGrade);
+
+void askUserForGrade();
+
+void printClasses();
+
+float calculateGPA();
 
 int main() {
 	setupMap();
+	addToGradeList("History", 3, "B+");
+	addToGradeList("History", 3, "B+");
+	addToGradeList("History", 3, "B+");
+	addToGradeList("History", 3, "B+");
+	addToGradeList("History", 3, "B+");
+	addToGradeList("History", 3, "B+");
+	addToGradeList("History", 3, "B+");
+	addToGradeList("History", 3, "B+");
+	float gpa = calculateGPA();
+	printClasses();
+	std::cout << "Your gpa is " << gpa << std::endl;
 }
 
 void setupMap() {
@@ -33,3 +59,33 @@ void setupMap() {
 	gpm["D"] = 1.00;
 	gpm["F"] = 0.00;
 }
+
+void addToGradeList(std::string name, int credit, std::string letterGrade) {
+	classInfo newClass = {name, credit, letterGrade};
+	classGradeList.push_back(newClass);
+}
+
+void assuserForGrade() {
+	//something
+}
+
+void printClasses() {
+	for (classInfo ci : classGradeList) {
+		std::cout << ci.className << std::endl;
+	}
+}
+
+float calculateGPA() {
+	float totalCredits = 1;
+	float totalGradePoints = 0.0;
+	for (classInfo ci : classGradeList) {
+		float gradePoint = gpm[ci.letterGrade];
+		totalCredits += ci.credit; 
+		totalGradePoints += ci.credit * gradePoint;
+	}
+	float gpa = totalGradePoints/totalCredits;
+	return gpa;
+}
+
+
+
